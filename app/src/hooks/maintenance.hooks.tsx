73d4e7b83139147e75c83d5e40837d5f01509181
bloop <diogofalardo04@gmail.com>
2026-09-23 10:37:11 +0100

    removed old content

--- app/src/hooks/maintenance.hooks.tsx
+++
@@ -1,9 +0,0 @@
-import { sfGetMaintenances } from "@/server/maintenance/maintenance.functions"
-import { useQuery } from "@tanstack/react-query"
-
-export function useGetMaintenacens() {
-  return useQuery({
-    queryKey: ["maintenances"],
-    queryFn: () => sfGetMaintenances(),
-  })
-}
--- app/src/hooks/metrics.hooks.tsx
+++
@@ -1,9 +0,0 @@
-import { sfMetrics } from "@/lib/metrics"
-import { useQuery } from "@tanstack/react-query"
-
-export function useMetrics() {
-  return useQuery({
-    queryKey: ["metrics"],
-    queryFn: () => sfMetrics(),
-  })
-}
